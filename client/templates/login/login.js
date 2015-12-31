34120867c326c884cc940b8806104895b32a6540
PerrettaAS <scottaperretta@gmail.com>
2015-12-31 16:56:34 -0500

    Add login/register template.

---
+++ client/templates/login/login.js
@@ -0,0 +1,16 @@
+Template.login.events({
+    'click #login-form-link': function(){
+        $("#login-form").delay(100).fadeIn(100);
+ 		$("#register-form").fadeOut(100);
+		$('#register-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+    },
+    'click #register-form-link': function(){
+        $("#register-form").delay(100).fadeIn(100);
+ 		$("#login-form").fadeOut(100);
+		$('#login-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+    },
+});
